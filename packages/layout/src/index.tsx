a354c675162feae2ccee381dd14a4b71a6bf6f76
光鱼 <guang.yu@yingdao.com>
2025-06-09 15:53:48 +0800

    feat: 添加通用前缀，防止样式 污染

--- packages/layout/src/index.tsx
+++ packages/layout/src/index.tsx
@@ -6,31 +6,55 @@ import {
   StoreContext,
 } from "./model";
 import useFixedLayoutStore from "./hooks/useFixedLayoutStore";
-import { Background, ReactFlow, ReactFlowProvider } from "@xyflow/react";
+import {
+  Background,
+  ReactFlow,
+  ReactFlowProvider,
+  useReactFlow,
+} from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
 import nodeTypes from "./components/NodeTypes";
 
-import "./index.css";
+import { useMount } from "ahooks";
+
+import "./reset.css";
 
 function FixedFlow() {
   const { nodes, edges } = useFixedLayoutStore();
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const { setCenter, getNode, getNodesBounds, getViewport } = useReactFlow();
   console.log("nodes", nodes);
   console.log("edges", edges);
+
+  useMount(() => {
+    const wrapper = wrapperRef.current;
+    if (wrapper) {
+      const { height } = wrapper.getBoundingClientRect();
+      setCenter(0, height / 2 - 200, {
+        zoom: 1,
+      });
+    }
+  });
   return (
-    <ReactFlowProvider>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        nodesConnectable={false}
-        zoomOnDoubleClick={false}
-        deleteKeyCode={null}
-        nodesDraggable={false}
-        nodeTypes={nodeTypes}
-      >
-        <Background />
-      </ReactFlow>
-    </ReactFlowProvider>
+    <div
+      className="fixed-flow-wrapper"
+      style={{ width: "100%", height: "100%" }}
+    >
+      <div ref={wrapperRef} className="w-full h-full">
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          nodesConnectable={false}
+          zoomOnDoubleClick={false}
+          deleteKeyCode={null}
+          nodesDraggable={false}
+          nodeTypes={nodeTypes}
+        >
+          <Background />
+        </ReactFlow>
+      </div>
+    </div>
   );
 }
 
@@ -43,9 +67,11 @@ export function FixedFlowLayout({ initialBlocks }: FixedLayoutModelConfig) {
     });
   }
   return (
-    <StoreContext.Provider value={storeRef.current}>
-      <FixedFlow />
-    </StoreContext.Provider>
+    <ReactFlowProvider>
+      <StoreContext.Provider value={storeRef.current}>
+        <FixedFlow />
+      </StoreContext.Provider>
+    </ReactFlowProvider>
   );
 }
 
