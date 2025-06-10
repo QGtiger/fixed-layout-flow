568a727b0681857a7292b276d40228a0134b24b3
光鱼 <guang.yu@yingdao.com>
2025-06-10 16:55:44 +0800

    feat: 配合 github action 进行访问

--- apps/fixedlayoutflow/vite.config.ts
+++ apps/fixedlayoutflow/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : "/fixedlayoutflow/",
+  base: process.env.NODE_ENV === "development" ? "/" : "/fixed-layout-flow/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
