e12ce700239da74e73f6fc344bcfaf19719c8f4e
光鱼 <guang.yu@yingdao.com>
2025-06-11 11:22:35 +0800

    feat: 新增 build:web 命令， 配合  npx xybot upload ./dist --dirName fixedlayoutflow 访问 web静态资源

--- apps/fixedlayoutflow/vite.config.ts
+++ apps/fixedlayoutflow/vite.config.ts
@@ -3,10 +3,22 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : "/fixed-layout-flow/",
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
+export default defineConfig(() => {
+  const isWebBuild = process.env.VITE_BUILD_MODE === "web";
+  if (isWebBuild) {
+    return {
+      base: "https://winrobot-pub-a.oss-cn-hangzhou.aliyuncs.com/static/fixedlayoutflow/dist/",
+      plugins: [react()],
+      resolve: {
+        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+      },
+    };
+  }
+  return {
+    base: process.env.NODE_ENV === "development" ? "/" : "/fixed-layout-flow/",
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+  };
 });
