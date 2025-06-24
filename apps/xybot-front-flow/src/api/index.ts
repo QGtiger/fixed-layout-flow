05f9c0d58508829744ba06d9ec6f6485c9aaaa57
光鱼 <guang.yu@yingdao.com>
2025-06-24 14:47:50 +0800

    feat: 添加 axios 依赖并实现 API 请求封装

---
+++ apps/xybot-front-flow/src/api/index.ts
@@ -0,0 +1,79 @@
+import axios from "axios";
+import { createMessage } from "@/utils/customMessage";
+import { navigate } from "@/utils/navigation";
+
+// 走控制台登录
+const ACCESS_TOKEN_KEY = "ACCESS_TOKEN";
+
+export function getAccessToken() {
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
+}
+
+export function setAccessToken(token: string) {
+  if (!token) {
+    return localStorage.removeItem(ACCESS_TOKEN_KEY);
+  }
+  localStorage.setItem(ACCESS_TOKEN_KEY, token);
+}
+
+export const client = axios.create({
+  // @ts-ignore
+  baseURL: process.env.API_URL || window.YD?.API_URL,
+});
+
+client.interceptors.request.use((config) => {
+  const headers = {
+    Authorization: `Bearer ${getAccessToken()}`,
+    "Xybot-Product": "rpa",
+  };
+
+  Object.assign(config.headers, headers);
+
+  return config;
+});
+
+client.interceptors.response.use(
+  // @ts-expect-error
+  (res) => {
+    const { code, data, msg } = res.data;
+    if (code === 200) {
+      return {
+        success: true,
+        data,
+        code,
+      };
+    } else {
+      if (code == 401) {
+        // 未登录
+        setAccessToken("");
+
+        navigate("/login");
+      }
+
+      createMessage(
+        {
+          type: "error",
+          content: msg,
+        },
+        {
+          hideOther: true,
+        }
+      );
+
+      return {
+        success: false,
+        data,
+        code,
+        errorMsg: msg,
+      };
+    }
+  },
+  (error) => {
+    return {
+      success: false,
+      data: null,
+      code: 500,
+      errorMsg: error.message,
+    };
+  }
+);
--- apps/xybot-front-flow/vite.config.ts
+++ apps/xybot-front-flow/vite.config.ts
@@ -1,41 +1,57 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { createHtmlPlugin } from 'vite-plugin-html';
-import { CodeInspectorPlugin } from 'code-inspector-plugin';
-import path from 'path';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { CodeInspectorPlugin } from "code-inspector-plugin";
+import path from "path";
 
 const YD = () => {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     return {};
   } else {
     return {
-      API_URL: process.env?.API_URL || 'https://test-api.yingdao.com'
+      API_URL: process.env?.API_URL || "https://test-api.yingdao.com",
     };
   }
 };
 
-export default defineConfig({
-  base: '/',
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      entry: '/src/.xybot/main.tsx',
-      viteNext: true,
-      inject: {
-        tags: [{ injectTo: 'head', tag: 'script', children: `window.YD=${JSON.stringify(YD())}` }]
-      }
-    }),
-    CodeInspectorPlugin({
-      bundler: 'vite'
-    })
-  ],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-      { find: 'xybot', replacement: path.resolve(__dirname, 'src/.xybot') }
-    ]
-  },
-  build: {
-    manifest: true
-  }
+export default defineConfig(({ mode }) => {
+  const env =
+    mode === "development" ? loadEnv(mode, process.cwd(), "") : process.env;
+
+  return {
+    base: "/",
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        entry: "/src/.xybot/main.tsx",
+        // @ts-expect-error 就这样吧
+        viteNext: true,
+        inject: {
+          tags: [
+            {
+              injectTo: "head",
+              tag: "script",
+              children: `window.YD=${JSON.stringify(YD())}`,
+            },
+          ],
+        },
+      }),
+      CodeInspectorPlugin({
+        bundler: "vite",
+      }),
+    ],
+
+    define: {
+      "process.env": env,
+    },
+    resolve: {
+      alias: [
+        { find: "@", replacement: path.resolve(__dirname, "src") },
+        { find: "xybot", replacement: path.resolve(__dirname, "src/.xybot") },
+      ],
+    },
+    build: {
+      manifest: true,
+    },
+  };
 });
