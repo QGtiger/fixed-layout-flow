dbc9e7f3af805487d29f6b115f7a44a5fb5450a8
光鱼 <guang.yu@yingdao.com>
2025-06-24 14:25:47 +0800

    feat: 新建 xybot-front-flow

---
+++ apps/xybot-front-flow/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'antd';
+const Test = () => {
+  return (
+    <div>
+      Test
+      <Button type="primary" className=" ml-5">
+        LLL
+      </Button>
+      <span className=" text-red-500">xxxx</span>
+    </div>
+  );
+};
+
+export default Test;
